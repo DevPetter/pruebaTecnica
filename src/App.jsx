8b5824aab71cbd9e90b59ce8139973573de92322
petter <petter0915@gmail.com>
2022-10-06 21:26:59 -0500

    Creación proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React from 'react'
+
+export const App = () => {
+  return (<>
+    <h1>PRUEBA TÉCNICA</h1>
+    <p>Pedro Quiñones Paternina</p>
+    <hr/>
+    </>
+  )
+}
+
