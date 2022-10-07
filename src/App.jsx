e07acc01b988d4025156d856441ed2bec7ff9a96
petter <petter0915@gmail.com>
2022-10-07 17:29:49 -0500

    Nuevo componente task con input y actualización de estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css';
 
-import React from 'react'
+import React, { useState } from 'react'
+import { Task } from './components/Task';
 
 export const App = () => {
+  const [tasks, setTasks] = useState([""])
   return (<>
     <h1>PRUEBA TÉCNICA</h1>
     <p>Pedro Quiñones Paternina</p>
-    <hr/>
+    <hr/> 
+    
+    <Task setTasks={setTasks}/>
+
+    <ol>
+      {tasks.map((task)=>{
+        return <li>{task}</li>
+      })}
+    </ol>
+
     </>
   )
 }
---
+++ src/components/Task.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+export const Task = ({setTasks}) => {
+    const [inputValue, setInputValue] = useState("Tarea")
+
+    const onInputValueChange = ({ target }) => {
+        setInputValue(target.value);
+      };
+
+      const onSubmit = (event)=>{
+        event.preventDefault();  //Evitar que el navegador web se refresque cada que se ingresa una tarea en el input
+        if(inputValue.trim().length <=1) return
+        setTasks((tasks)=>[inputValue, ...tasks]) //Actualizar el estado en tareas
+        setInputValue("")//Limpiar la caja de texto por medio de nuestro estado
+      }
+
+  return (
+      <form onSubmit={(event)=> onSubmit(event)}>
+        {/* Formulario con el input */}
+        <div>Tareas</div>
+        <input
+        type="text"
+        placeholder="Add task"
+        value={inputValue}
+        onChange={onInputValueChange}
+      />
+    </form>
+  )
+}
